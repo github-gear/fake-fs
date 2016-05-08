54e160d92b3288eae68ce34cf2b5cb21097f2efd
call-me-X <don-look-for-me@gmail.com>
2016-05-08 14:00:01 +0800

    ok

---
+++ spec/fail_test_spec.rb
@@ -0,0 +1,10 @@
+RSpec.describe "Game" do
+  describe "#score" do
+    it "goal!" do
+      expect("A").to eq("A")
+    end
+    it "goal goal!" do
+      expect("AA").to eq("AB")
+    end
+  end
+end
